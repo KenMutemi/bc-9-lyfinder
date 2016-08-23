d1d995fb7358871ba5e6346d5dee6461ee73d0ef
kennedy <mutemikennedy@gmail.com>
2016-08-23 17:14:58 +0300

    [app.py] Can pass song find command
    Enable song find command.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-"""Lyfinder.
+"""
+Lyfinder.
 
 Usage:
-  app.py 'song find' <search_query_string>
-  app.py 'song view' <song_id>
-  app.py 'song save' <song_id>
-  app.py 'song clear'
-  app.py (-h | --help)
+  app.py song find <search_query_string>
 
 Options:
-  -h --help     Show this screen.
+  -h, --help    Show this message.
 """
 
 from docopt import docopt
 
+def song_find(search_query_string):
+    print("song find, {0}".format(search_query_string))
+
 if __name__ == '__main__':
     arguments = docopt(__doc__)
+
+    # If an argument is was passed, execute its logic
+    if arguments['song'] and arguments['find']:
+        song_find(arguments['<search_query_string>'])
