7220ec15141bf8b190f837817b332eb3b98683ac
kennedy <mutemikennedy@gmail.com>
2016-08-23 17:28:48 +0300

    [app.py] Add all commands
    Add all commands for the application.

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ Lyfinder.
 
 Usage:
   app.py song find <search_query_string>
+  app.py song view <song_id>
+  app.py song save <song_id>
+  app.py song clear
 
 Options:
   -h, --help    Show this message.
@@ -13,9 +16,27 @@ from docopt import docopt
 def song_find(search_query_string):
     print("song find, {0}".format(search_query_string))
 
+def song_view(song_id):
+    print("song view, {0}".format(song_id))
+
+def song_save(song_id):
+    print("song save, {0}".format(song_id))
+
+def song_clear():
+    print("Clear all songs!")
+
 if __name__ == '__main__':
     arguments = docopt(__doc__)
 
     # If an argument is was passed, execute its logic
     if arguments['song'] and arguments['find']:
         song_find(arguments['<search_query_string>'])
+
+    elif arguments['song'] and arguments['view']:
+        song_view(arguments['<song_id>'])
+
+    elif arguments['song'] and arguments['save']:
+        song_save(arguments['<song_id>'])
+
+    elif arguments['song'] and arguments['clear']:
+        song_clear()
