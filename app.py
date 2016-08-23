600c3b4c46a360da548935c82f3a98b9074521b7
kennedy <mutemikennedy@gmail.com>
2016-08-23 18:53:48 +0300

    [app.py] Load credentials
    Add function to load Genius API credentials

--- app.py
+++ app.py
@@ -11,10 +11,27 @@ Options:
   -h, --help    Show this message.
 """
 
+import re
+
 from docopt import docopt
+
+def load_credentials():
+    lines = [line.rstrip('\n') for line in open('credentials.ini')]
+    chars_to_strip = "\'\""
+    for line in lines:
+        if "client_id" in line:
+            client_id = re.findall(r'[\"\']([^\"\']*)[\"\']', line)[0]
+
+        if "client_secret" in line:
+            client_secret = re.findall(r'[\"\']([^\"\']*)[\"\']', line)[0]
+
+        if "client_access_token" in line:
+            client_access_token = re.findall(r'[\"\']([^\"\']*)[\"\']', line)[0]
+    return client_id, client_secret, client_access_token
 
 def song_find(search_query_string):
     """Print the lyrics that match the search query."""
+
     print("song find, {0}".format(search_query_string))
 
 def song_view(song_id):
