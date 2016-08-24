dde30cf02e1353cb717712214cb2e1dcba938c04
kennedy <mutemikennedy@gmail.com>
2016-08-24 12:28:56 +0300

    [app.py, requirements.txt] Enable song selection functionality
    The user can now get song lyrics by providing the song ID.
    
    Closed #6

--- app.py
+++ app.py
@@ -15,8 +15,10 @@ import re
 import json
 import socket
 import urllib2
+import unicodedata
 
 from docopt import docopt
+from pprint import pprint
 from tabulate import tabulate
 
 CLIENT_ACCESS_TOKEN = 'Z1QtoNKtcX4F7ruB2QRaBnOK5n1SZNkOglv75XH7UvOSREikN6FceDaZoQLZBeyq'
@@ -50,21 +52,49 @@ def song_find(search_query_string):
     json_obj = json.loads(raw)
     body = json_obj["response"]["hits"]
 
+    # Instantiate list to hold all the lyrics search results.
     lyrics = []
    
     for result in body:
+        # Append each lyric property to a list and append that
+        # list to the lyrics list.
         lyric = []
         lyric.append(result["result"]["id"])
         lyric.append(result["result"]["title"])
         lyric.append(result["result"]["primary_artist"]["name"])
         lyrics.append(lyric)
+    # Tabulate our output...
     print tabulate(lyrics, ["ID", "Title", "Artist",], tablefmt="fancy_grid")
 
-        #print("{0} - {1} - {2}".format(result["result"]["id"], result["result"]["title"], result["result"]["primary_artist"]["name"]))
-
 def song_view(song_id):
-    """Print the lyrics of the song ID provided."""
-    print("song view, {0}".format(song_id))
+    """View song lyrics based on the song ID provided.
+
+    Parameters
+    ----------
+    song_id: int
+    The integer to use to get the song lyrics.
+
+    Prints
+    -------
+    referent: json_obj
+    Result for the parsed response.
+    """
+    querystring = "http://api.genius.com/referents?song_id={0}".format(song_id)
+    request = urllib2.Request(querystring)
+    request.add_header("Authorization", "Bearer " + CLIENT_ACCESS_TOKEN)
+    request.add_header("User-Agent", "curl/7.9.8 (i686-pc-linux-gnu) libcurl 7.9.8 (OpenSSL 0.9.6b) (ipv6 enabled)")
+
+    try:
+        response = urllib2.urlopen(request, timeout=4)
+        raw = response.read()
+    except socket.timeout:
+        pprint("Sorry, timed out. Try again later")
+    
+    json_obj = json.loads(raw)
+    
+    # All anotatable contents on Genius are called referents
+    for referent in json_obj['response']['referents']:
+        print referent['fragment'].rstrip()
 
 def song_save(song_id):
     """Save the lyrics of the song ID provided."""
