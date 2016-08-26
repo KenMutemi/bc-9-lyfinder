b478427da2884da1dbea1d6627d3b0a8e2c269c4
kennedy <mutemikennedy@gmail.com>
2016-08-26 07:15:18 +0300

    [app.py] Enable interactivity
    Made the app interactive.
    
    closes #14

---
+++ helpers.py
@@ -0,0 +1,13 @@
+class StringFormatter:
+    """This is a class for formatting (e.g. beautifying) our
+    string output.
+    """
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
